c74b910b6c6f6cf7575bd8b957cf50413a66bdc9
hrim <ihr931218@naver.com>
2024-01-24 22:18:08 +0900

    first

---
+++ main.py
@@ -0,0 +1,21 @@
+import streamlit as st
+from manim.utils.file_ops import open_file
+from manim_name import NameMerger
+from manim import config
+config.video_dir = "./"
+config.output_file = "piui"
+config.format = "mp4"
+config.quality = "low_quality"
+
+st.markdown("<h1 style='text-align: center; color: white;'>이름 궁합 계산기</h1>", unsafe_allow_html=True)
+st.markdown("<h3 style='text-align: center; color: #555555;'>commit.im@gmail.com</h3>", unsafe_allow_html=True)
+input_string1 = st.text_input("이름 1")
+input_string2 = st.text_input("이름 2")
+if st.button("Match name"):
+    if not (input_string1 and input_string2):
+        st.warning("한글 이름을 입력해주세요", icon="⚠️")
+    else:
+        name_merger = NameMerger(input_string1, input_string2)
+        name_merger.render()
+        video_bytes = open("piui.mp4", "rb").read()
+        st.video(video_bytes)
